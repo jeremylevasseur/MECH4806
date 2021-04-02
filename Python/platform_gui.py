ed0a7a216eb06ce9d76d7c5615a3d0606cb42c73
jeremylevasseur <jeremy.levasseur.1997@gmail.com>
2021-04-02 18:29:10 -0400

    629

--- Python/platform_gui.py
+++ Python/platform_gui.py
@@ -4,20 +4,23 @@ import custom_tkinter_functions
 
 screenWidth = 900
 screenHeight = 900
-# screenHeight = (screenWidth/2) * math.tan(math.radians(60))
+
+sideLength = 600
 
 margin = 120
 centerRadius = 30
-
+outlineRadius = sideLength
 
 x1 = screenWidth / 2
 y1 = margin
 
-x2 = margin
-y2 = (screenWidth/2) * math.tan(math.radians(60)) - margin
+x2 = x1 - (sideLength*math.sin(math.radians(30)))
+y2 = y1 + (sideLength*math.cos(math.radians(30)))
 
-x3 = screenWidth - margin
-y3 = (screenWidth/2) * math.tan(math.radians(60)) - margin
+x3 = x1 + (sideLength*math.sin(math.radians(30)))
+y3 = y1 + (sideLength*math.cos(math.radians(30)))
+
+triangleHeight = (sideLength/2) * math.tan(math.radians(60))
 
 root= Tk()
 can = Canvas(root, width=screenWidth, height=screenHeight)
@@ -26,6 +29,6 @@ can.pack()
 
 points = [x1,y1, x2,y2, x3,y3]
 can.create_polygon(points, fill='black')
-can.create_oval((screenWidth/2) - centerRadius, (screenHeight/2) - centerRadius, (screenWidth/2) + centerRadius, (screenHeight/2) + centerRadius, outline="#f11", fill="red", width=2)
-can.create_oval((screenWidth/2) - centerRadius, (screenHeight/2) - centerRadius, (screenWidth/2) + centerRadius, (screenHeight/2) + centerRadius, outline="#f11", fill="red", width=2)
+can.create_oval((screenWidth/2) - centerRadius, y1 + (triangleHeight*2/3) - centerRadius, (screenWidth/2) + centerRadius, y1 + (triangleHeight*2/3) + centerRadius, fill="red", width=2)
+can.create_oval((screenWidth/2) - (triangleHeight*2/3), y1 + (triangleHeight*2/3) - (triangleHeight*2/3), (screenWidth/2) + (triangleHeight*2/3), y1 + (triangleHeight*2/3) + (triangleHeight*2/3), outline="red", width=2)
 root.mainloop()
